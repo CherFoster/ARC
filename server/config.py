13572a4eb2b4d67f37f22d3fb687ed5bfd8a85e6
Cherise Foster <cherise.foster@outlook.com>
2023-10-11 13:51:15 -0600

    created models.py

--- server/config.py
+++ server/config.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from sqlalchemy import MetaData
 
 # Local imports
@@ -16,6 +17,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+app.secret_key = '5fcb0d240c778d65e16da3e77d985ba78857e5517c99827f3e7e217439c6b2df'
+
 # Define metadata, instantiate db
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -23,6 +26,8 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
+
+bcrypt = Bcrypt(app)
 
 # Instantiate REST API
 api = Api(app)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,112 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import db, bcrypt
+from datetime import datetime
 
-from config import db
+assignment = db.Table('assignment', 
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('house_id', db.Integer, db.ForeignKey('houses.id'))
+)
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
 
-# Models go here!
+    serialize_rules = ('-_password_hash',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    agency = db.Column(db.String)
+    image = db.Column(db.String)
+
+    houses = db.relationship('House', secondary=assignment, back_populates='users')
+
+    notes = db.relationship('Note', back_populates='user')
+  
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+    
+    # Takes password, hashes it, and stores the hash
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    # compares hashed password to the stored hash  
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return (
+            f'User: {self.username}, ' \
+            + f'Agency: {self.agency}, ' \
+            + f'Image: {self.image} ' \
+        )
+    
+class House(db.Model, SerializerMixin):
+    __tablename__ = "houses"
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    occupants = db.Column(db.Integer)
+    animals = db.Column(db.Integer)
+    evacuation_status_id = db.Column(db.Integer, db.ForeignKey('evac_status.id'), unique=True)
+
+    evacuation_status = db.relationship('EvacuationStatus', uselist=False)
+
+    users = db.relationship('User', secondary=assignment, back_populates='house')
+
+    notes = db.relationship('Note', back_populates='house')
+
+    def __repr__(self):
+        return (
+            f'Address: {self.address}, ' \
+            + f'Latitude: {self.latitude}, ' \
+            + f'Longitude: {self.longitude} ' \
+            + f'Number of Occupants: {self.occupants}, ' \
+            + f'Number of Animals: {self.animals}, ' \
+        )
+    
+class EvacuationStatus(db.Model, SerializerMixin):
+    __tablename__ = "evac_status"
+
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    house = db.relationship('House', back_populates='evacuation_status')
+
+    def __repr__(self):
+        return f'<Evacuation Status: {self.status} at {self.timestamp}>'
+    
+    @validates('status')
+    def validate_status(self, key, status):
+        if not status or not isinstance(status, str):
+            raise ValueError('Status must not be None or be empty')
+        return status
+    
+class Note(db.Model, SerializerMixin):
+    __tablename__ = "notes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    details = db.Column(db.String)
+    medical_conditions = db.Column(db.String)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'))
+
+    user = db.relationship('User', back_populates='notes')
+    house = db.relationship('House', back_populates='notes')
+
+    def __repr__(self):
+        return (
+            f'Details: {self.details}, ' \
+            + f'Medical Condition: {self.medical_conditions}' \
+        )
+    
