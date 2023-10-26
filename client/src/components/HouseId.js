499fded3d992029bb5e1e4c00c491a9078dc537a
Cherise Foster <cherise.foster@outlook.com>
2023-10-26 11:42:03 -0600

    added notes to houseid

--- client/src/components/HouseId.js
+++ client/src/components/HouseId.js
@@ -6,6 +6,7 @@ import '../styles/HouseIdButton.css';
 function HouseId(){
     const {id} = useParams();
     const [house, setHouse] = useState(null);
+    const [notes, setNotes] = useState([]);
 
     useEffect(() => {
         fetch(`/api/houses/${id}`)
@@ -13,6 +14,11 @@ function HouseId(){
         .then(data => {
             setHouse(data);
         });
+        fetch(`/api/houses/${id}/notes`)
+            .then(res => res.json())
+            .then(data => {
+                setNotes(data);
+            });
     },[id])
 
     if (!house) return <div>Loading...</div>
@@ -21,8 +27,17 @@ function HouseId(){
         <Card style={{ width: '70rem'}}>
             <Card.Body>
                 <Card.Title style={{fontSize: '30px'}}>{house.house_number} {house.street_name}, {house.city}, {house.zip_code}</Card.Title>
-                <Card.Text style={{ color: 'black', fontSize: '23px'}}>
-                    Notes
+                <Card.Text style={{ color: 'black', fontSize: '23px' }}>
+                    Notes:
+                    {notes.map(note => (
+                        <p style={{color: 'black', fontSize: '15px'}} key={note.id}>
+                            <u>Details:</u> {note.details}
+                            <br/> Time: {note.timestamp}
+                        </p>
+                    ))}
+                    <p style={{color: 'black', fontSize: '15px'}}>
+                        <u>Medical Conditions:</u> {notes.reduce((acc, note) => acc + note.medical_conditions + ". ", "")}
+                    </p>
                 </Card.Text>
                 <ListGroup className="list-group-flush">
                     <ListGroup.Item>Latitude: {house.latitude}</ListGroup.Item>
